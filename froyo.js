7b65a9fb300f1bea8e8b3ca5bd440f04e8cd8b34
Sonia Gupta <soniagupta@Sonias-Air.attlocal.net>
2025-03-25 00:03:31 -0400

    Initial commit

---
+++ froyo.js
@@ -0,0 +1,30 @@
+//Prompt users for list of froyo flavors separated by a comma
+const userString = prompt (
+    "Please enter desired froyo orders separated by commas.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+
+//Split user's array into an array of strings
+const stringArray = userString.split(",");
+
+//Creates function to iterate through array of flavors 
+function countOrders(flavors) {
+    let totalOrders = {}; // Creates object to store counts
+    
+    //Loops through the array and updates object values accordingly
+    for (const froyoFlavor of flavors) {
+        if (totalOrders[froyoFlavor]) {
+            totalOrders[froyoFlavor]++;
+        } else {
+            totalOrders[froyoFlavor] = 1;
+        }
+    }
+    
+    return totalOrders; //Function returns object
+}
+
+//Calls the function and stores result in variable
+const orderSummary = countOrders(stringArray);
+
+//Prints output to console in table
+console.table(orderSummary);
